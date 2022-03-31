725dd58feb327c9e7df190879ca482906b74824b
HyeongJin <khj7741@gmail.com>
2022-04-01 04:43:39 +0900

    코드 개선 play / stop button, game 초기화, random positioning

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ const playBtn = document.querySelector(".timer__playBtn");
 const stopBtn = document.querySelector(".timer__stopBtn");
 const time = document.querySelector(".timer__time");
 const restartBtn = document.querySelector(".restart__button");
-const carrotContainer = document.querySelector("#carrot");
+const gameField = document.querySelector("#gameField");
 const bugsContainer = document.querySelector("#bug");
-const resultLost = document.querySelector("#result");
+const result = document.querySelector("#result");
 const carrotCountBox = document.querySelector(".carrotCount");
-const resultWin = document.querySelector("#resultWin");
 const resultContainer = document.querySelector("#resultContainer");
 
+//sound
 const pullCarrotSound = new Audio(`sound/carrot_pull.mp3`);
 const pullBugSound = new Audio(`sound/bug_pull.mp3`);
 const winSound = new Audio(`sound/game_win.mp3`);
@@ -20,51 +20,52 @@ let carrotCount;
 let ispaused = false;
 let intervalId;
 function countDown() {
+  ispaused = true;
   if (ispaused && startSeconds) {
     startSeconds--;
   }
   time.innerHTML = `${startSeconds}초`;
-  !startSeconds && loseGame();
+  !startSeconds && winOrLose(`lost`);
+}
+function showPlayBtn() {
+  stopBtn.classList.add("hide");
+  playBtn.classList.remove("hide");
+}
+function showStopBtn() {
+  playBtn.classList.add("hide");
+  stopBtn.classList.remove("hide");
+}
+function addItem(className, imgsrc, count, imgSize) {
+  const maxWidth = gameField.getBoundingClientRect().width;
+  const maxHeight = gameField.getBoundingClientRect().height;
+  for (let i = 0; i < count; i++) {
+    const x = Math.floor(Math.random() * (maxWidth - imgSize));
+    const y = Math.floor(Math.random() * (maxHeight - imgSize));
+    const item = document.createElement("img");
+    item.src = `${imgsrc}`;
+    gameField.appendChild(item);
+    item.classList.add(className);
+    item.id = `${className}${i}`;
+    item.style.transform = `translate(${x}px,${y}px)`;
+  }
 }
 
 playBtn.addEventListener("click", () => {
   alertSound.play();
   carrotCount = 10;
   carrotCountBox.innerHTML = `${carrotCount}`;
-  const maxWidth = document.body.clientWidth;
-  const maxHeight = document.body.clientHeight;
-  ispaused = true;
+
   if (!intervalId) {
     intervalId = setInterval(countDown, 1000);
   }
-  playBtn.classList.add("hide");
-  stopBtn.classList.remove("hide");
+  showStopBtn();
 
-  if (carrotContainer.classList.contains(`show`)) {
+  if (gameField.classList.contains(`show`)) {
     return;
   } else {
-    carrotContainer.classList.add(`show`);
-    for (let i = 0; i < 10; i++) {
-      const x = Math.floor(Math.random() * (maxWidth - 80));
-      const y = Math.floor(Math.random() * (maxHeight - 80));
-      const x2 = Math.floor(Math.random() * (maxWidth - 50));
-      const y2 = Math.floor(Math.random() * (maxHeight - 50));
-      const carrots = document.createElement("img");
-      carrots.src = "img/carrot.png";
-      carrotContainer.appendChild(carrots);
-      carrots.classList.add(`carrots-position`);
-      carrots.id = `carrots-pisition${i}`;
-      const carrotsArr = document.querySelectorAll(`.carrots-position`);
-      carrotsArr[i].style.transform = `translate(${x}px,${y}px)`;
-
-      const bugs = document.createElement("img");
-      bugs.src = "img/bug.png";
-      bugsContainer.appendChild(bugs);
-      bugs.classList.add(`bugs-position`);
-      bugs.id = `bugs-pisition${i}`;
-      const bugsArr = document.querySelectorAll(`.bugs-position`);
-      bugsArr[i].style.transform = `translate(${x2}px,${y2}px)`;
-    }
+    gameField.classList.add(`show`);
+    addItem(`carrot`, `img/carrot.png`, 10, 80);
+    addItem(`bug`, `img/bug.png`, 10, 50);
   }
 });
 
@@ -82,37 +83,34 @@ stopBtn.addEventListener("click", (e) => {
 // restartBtn.addEventListener("click", () => {});
 
 window.addEventListener("click", (e) => {
-  if (e.target.className == "carrots-position") {
+  if (e.target.className == "carrot") {
     const targetElement = document.querySelector(`#${e.target.id}`);
     pullCarrotSound.play();
-    carrotContainer.removeChild(targetElement);
+    gameField.removeChild(targetElement);
     carrotCount--;
     carrotCountBox.innerHTML = `${carrotCount}`;
   }
-  if (e.target.className == "bugs-position") {
+  if (e.target.className == "bug") {
     pullBugSound.play();
-    loseGame(e);
+    winOrLose(`lost`);
   }
   if (carrotCount == 0) {
-    winGame();
+    winOrLose(`win`);
+    winSound.play();
   }
 });
-
-function loseGame(e) {
-  resultLost.classList.remove(`hide`);
-  resultLost.classList.add(`displayResult`);
+function winOrLose(gameresult) {
+  clearInterval(intervalId);
+  resultContainer.innerHTML = `<section id="result">
+  <div class="result__${gameresult}">you ${gameresult}</div>
+  <button class="restart__button">
+    <i class="fa-solid fa-rotate-left"></i>
+  </button>
+  </section>
+  `;
   stopBtn.classList.add("hide");
   playBtn.classList.remove("hide");
   ispaused = false;
-}
-
-function winGame() {
-  resultWin.classList.remove(`hide`);
-  resultWin.classList.add(`displayResult`);
-  stopBtn.classList.add("hide");
-  playBtn.classList.remove("hide");
-  ispaused = false;
-  winSound.play();
 }
 
 function readyToRegame() {
@@ -124,32 +122,18 @@ function readyToRegame() {
   time.innerHTML = `${startSeconds}초`;
   carrotCount = 1;
   carrotCountBox.innerHTML = `0`;
-  carrotContainer.classList.remove(`show`);
-  while (carrotContainer.hasChildNodes()) {
-    carrotContainer.removeChild(carrotContainer.firstChild);
-  }
-  while (bugsContainer.hasChildNodes()) {
-    bugsContainer.removeChild(bugsContainer.firstChild);
-  }
-  stopBtn.classList.add("hide");
-  playBtn.classList.remove("hide");
+  gameField.classList.remove(`show`);
+  resultContainer.innerHTML = "";
+  gameField.innerHTML = "";
+  showPlayBtn();
 }
 
 resultContainer.addEventListener("click", (e) => {
   console.log(e.target.className);
   if (
     e.target.className == `restart__button` ||
-    e.target.classList.contains(`restart__i--lost`)
+    e.target.classList.contains(`fa-rotate-left`)
   ) {
     readyToRegame();
-    resultLost.classList.add(`hide`);
-    resultLost.classList.remove(`displayResult`);
-  } else if (
-    e.target.className == `resultWin__button` ||
-    e.target.classList.contains(`restart__i--win`)
-  ) {
-    readyToRegame();
-    resultWin.classList.add(`hide`);
-    resultWin.classList.remove(`displayResult`);
   }
 });
